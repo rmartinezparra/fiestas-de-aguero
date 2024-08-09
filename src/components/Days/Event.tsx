a9e6618cbd488547f75721b142459fdf2f41e474
Reikand <r.martinezparra@outlook.com>
2024-08-09 14:44:11 +0200

    chore: Update font sizes and styles for Event component
    This commit updates the font sizes and styles for the Event component in the Days directory. The font sizes have been adjusted to improve readability, and the font styles have been updated to use the JosefinSlab font. These changes enhance the visual appearance of the component and ensure consistency with the overall theme.

--- src/components/Days/Event.tsx
+++ src/components/Days/Event.tsx
@@ -4,8 +4,8 @@ const Event = ({ data }: { data: ContentEventType }) => {
 
   return (
     <div className='flex flex-row space-x-6 h-auto w-auto p-3'>
-      <p className='h-full xl:w-[60px] w-[40px] text-center place-content-center text-base lg:text-xl xl:text-2xl font-josefinLight'>{data.time}</p>
-      <p className='h-auto text-justify place-content-center text-base lg:text-xl xl:text-2xl w-full font-josefinRegular'>{data.description}</p>
+      <p className='h-full xl:w-[60px] w-[40px] text-center place-content-center text-base lg:text-xl xl:text-xl font-josefinLight'>{data.time}</p>
+      <p className='h-auto text-justify place-content-center text-base lg:text-xl xl:text-xl w-full font-josefinRegular'>{data.description}</p>
     </div>
   );
 };
